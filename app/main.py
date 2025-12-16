30c78dc0cf36b308274e0732e0641a776bf96d3d
Elen Hayot <elen.hayot@gmail.com>
2025-12-16 10:29:19 +0100

    alembic init + user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from app.core.database import Base, engine
+
+from app.models import user
 
 app = FastAPI(title="Bills API")
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def healthcheck():
